9bd4d068156f5033a9dcf39db887817082d9368c
Rohit Bhati <rohitbhati8833@gmail.com>
2021-03-13 15:04:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Finance-Manager-Project
+ A website made using HTML5, CSS3, Animate CSS
---
+++ app.js
@@ -0,0 +1,15 @@
+$(".nav-link").click(function() {
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+    ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 250)
+            return false;
+        }
+}})
